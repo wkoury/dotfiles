efa29bec427f8d1c7c8b72fb8e363c273a5252ec
Will Koury <wkoury@hey.com>
2020-11-02 07:59:17 -0600

    Adds script to install homebrew and oh my zsh

---
+++ scripts/install.sh
@@ -0,0 +1,6 @@
+ ln -s documents/dotfiles/brew
+ ln -s documents/dotfiles/.oh-my-zsh
+
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
