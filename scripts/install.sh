ee0076a399bfe21cfe47f8a497bb55be7ea44663
Will Koury <wkoury@hey.com>
2021-01-06 09:45:22 -0500

    Augments install script

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,5 +1,6 @@
- ln -s documents/dotfiles/brew
- ln -s documents/dotfiles/.oh-my-zsh
+ ln dotfiles/zsh/.zshrc
+ ln dotfiles/zsh/.p10k.zsh
+ ln -s dotfiles/zsh/.oh-my-zsh
 
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 
