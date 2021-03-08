054f6d2f43b61d60e52ea667cbb53828a5ecfb81
Will Koury <wkoury@hey.com>
2021-03-08 18:40:12 -0500

    Update links script

--- scripts/links.sh
+++ scripts/links.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
-cd ~
-ln dotfiles/zsh/.zshrc .zshrc
-ln dotfiles/zsh/.p10k.zsh .p10k.zsh
-ln -s dotfiles/zsh/.oh-my-zsh .oh-my-zsh
+
+ln ~/dotfiles/zsh/.zshrc ~/.zshrc
+ln ~/dotfiles/zsh/.p10k.zsh ~/.p10k.zsh
+ln ~/dotfiles/vim/.vimrc ~/.vimrc
+ln -s ~/.vimrc ~/.config/nvim/init.vim
+
+ln -s ~/dotfiles/zsh/.oh-my-zsh ~/.oh-my-zsh
+
